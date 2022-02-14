ab9771cdad2689a3779a1d27afb8dba67b4e37d7
marziaf <maarzia.f@gmail.com>
2022-02-14 16:53:14 +0100

    The principles to draw the directions are wrong, because the convergence point of the lines is not 0,0

--- src/showVps.m
+++ src/showVps.m
@@ -22,11 +22,18 @@ s = [3291, 1830];
 colors = ["red", "green", "blue"];
 
 % overlay extracted directions
-for d = 1: 6% TODOsize(data, 2)
+for d = 1: 1% TODOsize(data, 2)
     load(fullfile(sourceImgDir, data(d).image + ".mat"));
     img = imread(fullfile(outDir, "jaccImg", data(d).image + ".png"));
-    vpHomo = data(d).vps;
-    dir = vpHomo(1:2,:);
+    %vpHomo = data(d).vps;
+    numVp = 0;
+    % TODO recalculate?
+    for k = 1:size(data(d).vps,2)
+        numVp = numVp + 1;
+        vp(:, numVp) = getClusterVPs(data(d).clusters(k).edges);
+    end
+    vpHomo = [ vp(2,:); vp(1,:); vp(3, :) ];
+    dir = vpHomo(1:2, :);
     numVps = size(dir,2);
     linesx = zeros(numVps, 2);
     linesy = zeros(numVps, 2);
@@ -37,10 +44,12 @@ for d = 1: 6% TODOsize(data, 2)
         [linesx(v, :), linesy(v, :)] = lineEqToSeg(a, b, c, s);
     end
 
-    f = figure(Visible="off"); set(0, 'currentfigure', f);
-    imshow(img), hold on, title(data(d).image), axis auto;
+    % f = figure(Visible="off"); set(0, 'currentfigure', f); %TODO
+    f = figure(Visible="on");
+    imshow(img), hold on, title(data(d).image), axis auto, axis equal;
     for k=1:numVps
         plot(linesx(k,:)', linesy(k, :)', Color=colors(k), LineWidth=5);
+        plot(vpHomo(2,k)/vpHomo(3,k), vpHomo(1,k)/vpHomo(3,k), Color=colors(k), Marker="+", MarkerSize=20)
     end
 
     if data(d).algorithm == algorithms.jaccard
@@ -48,8 +57,9 @@ for d = 1: 6% TODOsize(data, 2)
     else
         outFile = fullfile(outAxTani, data(d).image);
     end
-    disp("Saving " + outFile)
-    saveas(f, outFile, 'png')
+    % TODO restore
+    %disp("Saving " + outFile)
+    %saveas(f, outFile, 'png')
 
 end
 
@@ -57,7 +67,7 @@ function [x, y] = lineEqToSeg(a, b, c, s)
     % segment of line ax + by + c = 0 in the range between [0 0] and s
     x = [ s(1) / 2, s(1)];
     if ( b ~= 0 && a ~= 0) % not vertical, not horizontal
-        y2 = -(a/b) * s(1) - c/b;
+        y2 = -(a/b) * x(2) - c/b;
         if y2 > s(2) || y2 < 0 % out of image
             if y2 > s(2)
                 y2 = s(2); 
